0537990ae647904a271a7f53ab2f942dfa6c8a33
vovak <ageev.evgen.2020@mail.ru>
2022-02-28 01:34:18 +0300

    add validate in controller and model

--- src/main/java/ru/ageev/companyregister/controller/ControllerCompany.java
+++ src/main/java/ru/ageev/companyregister/controller/ControllerCompany.java
@@ -3,10 +3,11 @@ package ru.ageev.companyregister.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.ageev.companyregister.model.Company;
 import ru.ageev.companyregister.service.ServiceCompany;
-
+import javax.validation.Valid;
 import java.util.List;
 @RestController
 @RequestMapping ("/company")
@@ -19,13 +20,15 @@ public class ControllerCompany {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> create(@RequestBody Company company) {
+    public ResponseEntity<?> createCompany(@Valid @RequestBody Company company,BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return new ResponseEntity<>("you didn't fill in the fields",HttpStatus.BAD_REQUEST);
         serviceCompany.create(company);
         return new ResponseEntity<>("id new company " + company.getId(),HttpStatus.CREATED);
 
     }
     @GetMapping("/{id}")
-    public ResponseEntity<?> readId(@PathVariable(name = "id") long id) {
+    public ResponseEntity<?> readCompanyId(@PathVariable(name = "id") long id) {
         final Company company = serviceCompany.read(id);
         return company != null
                 ? new ResponseEntity<>(company, HttpStatus.OK)
@@ -33,7 +36,7 @@ public class ControllerCompany {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Company>> readAll() {
+    public ResponseEntity<List<Company>> readAllCompanies() {
         final List<Company> companies = serviceCompany.readAll();
 
         return companies != null &&  !companies.isEmpty()
@@ -41,16 +44,19 @@ public class ControllerCompany {
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable(name = "id") long id, @RequestBody Company company) {
-        final boolean updated = serviceCompany.update(company, id);
+    public ResponseEntity<?> updateCompany(@PathVariable(name = "id") long id,@Valid
+                                            @RequestBody Company company,BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return new ResponseEntity<>("not modified",HttpStatus.NOT_MODIFIED);
 
+        final boolean updated = serviceCompany.update(company,id);
         return updated
                 ? new ResponseEntity<>(HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable(name = "id") long id){
+    public ResponseEntity<?> deleteCompany(@PathVariable(name = "id") long id){
         final boolean deleted = serviceCompany.delete(id);
         return deleted
                 ? new ResponseEntity<>(HttpStatus.OK)
